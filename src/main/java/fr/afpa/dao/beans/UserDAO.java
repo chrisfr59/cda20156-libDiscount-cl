d2bdc4f4fb8c1e1879c2f93f4d250d83fd426c27
Christian <clohez28@gmail.com>
2020-11-13 10:53:54 +0100

    ModifBeanUser-CL

--- src/main/java/fr/afpa/dao/beans/UserDAO.java
+++ src/main/java/fr/afpa/dao/beans/UserDAO.java
@@ -1,10 +1,14 @@
 package fr.afpa.dao.beans;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,11 +20,12 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 
-@Entity(name = "user")
+@Entity(name = "utilisateur")
 public class UserDAO {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
 	private int id_user;
 	
 	@Column(unique = true)
@@ -29,23 +34,33 @@ public class UserDAO {
 	private String nom;
 	private String prenom;
 	private String nomLibrairie;
-	private String adresse;
 	private String mail;
-	private long telephone;
+	private String telephone;
+	private boolean admin;
 	
-	
-	public UserDAO(String pseudo, String pwd, String nom, String prenom, String nomLibrairie, String adresse,
-			String mail, long telephone) {
+	@ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_adresse", referencedColumnName = "id_adresse")
+	private AdresseDAO adresse;
+
+	public UserDAO(String pseudo, String pwd, String nom, String prenom, String nomLibrairie, String mail,
+			String telephone, boolean admin, AdresseDAO adresse) {
 		super();
 		this.pseudo = pseudo;
 		this.pwd = pwd;
 		this.nom = nom;
 		this.prenom = prenom;
 		this.nomLibrairie = nomLibrairie;
-		this.adresse = adresse;
 		this.mail = mail;
 		this.telephone = telephone;
+		this.admin = admin;
+		this.adresse = adresse;
 	}
+	
+	
+
+	
+	
+
 	
 	
 	
