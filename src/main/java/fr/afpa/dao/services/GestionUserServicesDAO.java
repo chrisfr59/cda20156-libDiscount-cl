ca7ce2954f5e41cf96c41e0ef20cad7117ff9f06
Christian <clohez28@gmail.com>
2020-11-16 09:04:02 +0100

    ClassServiceDAO-CL

--- src/main/java/fr/afpa/dao/services/GestionUserServicesDAO.java
+++ src/main/java/fr/afpa/dao/services/GestionUserServicesDAO.java
@@ -1,5 +1,7 @@
 package fr.afpa.dao.services;
 
+import javax.persistence.Query;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -9,29 +11,40 @@ import fr.afpa.session.HibernateUtils;
 
 public class GestionUserServicesDAO {
 
-	
-	public static Boolean insertUser(AdresseDAO adresse,UserDAO user) {
+	public static Boolean insertUser(AdresseDAO adresse, UserDAO user) {
 		// TODO Auto-generated method stub
 		try {
 			// Ouverture d'une session Hibernate
-			  Session s = HibernateUtils.getSession();
-			    	// Début de la transaction
-			  Transaction tx = s.beginTransaction();
-			    			
-			  // Enregistrements de l'objet personne
-			  s.persist(adresse);  			
-			  s.persist(user);
-			    			
-			     	    
-			    			tx.commit();
-			    			s.close();
-			    			
+			Session s = HibernateUtils.getSession();
+			// Début de la transaction
+			Transaction tx = s.beginTransaction();
+
+			// Enregistrements de l'objet personne
+			s.persist(adresse);
+			s.persist(user);
+
+			tx.commit();
+			s.close();
+
 		} catch (Exception e) {
-		 return false;
+			return false;
 		}
 		return true;
-		
-	}
 
+	}
+	
+	public UserDAO recupPersonneParPseudo(String pseudo) {
+		// Ouverture d'une session Hibernate
+				Session		s = HibernateUtils.getSession();
+				
+				Query q = s.getNamedQuery("trouverPersonneParSonPseudo");
+				
+			    q.setParameter("val", pseudo);
+				
+				UserDAO user = (UserDAO) q.getSingleResult();
+				
+				s.close();
+				return user;
+	}
 
 }
